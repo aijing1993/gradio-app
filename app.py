d87bf86e96ea18df3f5ed768661826ab64fd85a3
aijing1993 <bitgaj20092351@gmail.com>
2024-09-14 00:02:10 +0000

    update permission

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
-os.system('chmod +x /home/ubuntu/genai-20240825')
+os.system('sudo chmod +x /home/ubuntu/genai-20240825')
 os.chdir('/home/ubuntu/genai-20240825/workspace/v2v_conda_pipeline')
 os.system("bash run_app.sh")
